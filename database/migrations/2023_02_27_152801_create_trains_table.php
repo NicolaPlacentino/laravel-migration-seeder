d0ea360bf564f90f6c0c122a9b3650f03ff427a4
NicolaPlacentino <nyko0921@gmail.com>
2023-02-27 17:08:18 +0100

    created trains table colums

--- database/migrations/2023_02_27_152801_create_trains_table.php
+++ database/migrations/2023_02_27_152801_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 30);
+            $table->string('departure_station', 50);
+            $table->string('arriving_station', 50);
+            $table->dateTime('departure_time');
+            $table->dateTime('arriving_time');
+            $table->string('train_code', 10);
+            $table->tinyInteger('wagon_number')->unsigned()->nullable();
+            $table->boolean('is_on_time')->default(1);
+            $table->boolean('is_cancelled')->default(0);
             $table->timestamps();
         });
     }
